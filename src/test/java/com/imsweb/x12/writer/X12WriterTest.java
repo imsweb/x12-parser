0825fca8b8a67fd9ef004a71577d363354641dab
angelasd <angelaszekd@imsweb.com>
2020-06-16 17:00:10 -0400

    starting unit test for printing string from loop (#33)

--- src/test/java/com/imsweb/x12/writer/X12WriterTest.java
+++ src/test/java/com/imsweb/x12/writer/X12WriterTest.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 
 import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
@@ -15,7 +16,11 @@ import org.jsoup.select.Elements;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.imsweb.x12.Element;
 import com.imsweb.x12.LineBreak;
+import com.imsweb.x12.Loop;
+import com.imsweb.x12.Segment;
+import com.imsweb.x12.Separators;
 import com.imsweb.x12.reader.X12Reader;
 import com.imsweb.x12.reader.X12Reader.FileType;
 
@@ -98,5 +103,58 @@ public class X12WriterTest {
         Assert.assertEquals(expected, writer.toX12String(lineBreak).trim());
     }
 
-
+    @Test
+    public void testPrintFromLoop() {
+        Separators separators = new Separators();
+        separators.setLineBreak(LineBreak.CRLF);
+        Loop isaLoop = new Loop(separators, "ISA_LOOP");
+        Segment segment = new Segment("ISA", separators);
+        addElement(segment, "01", "00", separators);
+        addElement(segment, "02", "          ", separators);
+        addElement(segment, "03", "01", separators);
+        addElement(segment, "04", "SECRET    ", separators);
+        addElement(segment, "05", "ZZ", separators);
+        addElement(segment, "06", "SUBMITTERS.ID  ", separators);
+        addElement(segment, "07", "ZZ", separators);
+        addElement(segment, "08", "RECEIVERS.ID   ", separators);
+        addElement(segment, "09", "030101", separators);
+        addElement(segment, "10", "1253", separators);
+        addElement(segment, "11", "U", separators);
+        addElement(segment, "12", "00501", separators);
+        addElement(segment, "13", "000000905", separators);
+        addElement(segment, "14", "1", separators);
+        addElement(segment, "15", "T", separators);
+        addElement(segment, "16", ":", separators);
+        isaLoop.addSegment(segment);
+        segment = new Segment("IEA", separators);
+        addElement(segment, "01", "1", separators);
+        addElement(segment, "02", "000000905", separators);
+        isaLoop.addSegment(segment);
+        
+        Loop gsLoop = new Loop(separators, "GS_LOOP");
+        segment = new Segment("GS", separators);
+        addElement(segment, "01", "HC", separators);
+        addElement(segment, "02", "SENDER CODE", separators);
+        addElement(segment, "03", "RECEIVER CODE", separators);
+        addElement(segment, "04", "19991231", separators);
+        addElement(segment, "05", "0802", separators);
+        addElement(segment, "06", "1", separators);
+        addElement(segment, "07", "X", separators);
+        addElement(segment, "08", "005010X222A1", separators);
+        gsLoop.addSegment(segment);
+        segment = new Segment("GE", separators);
+        addElement(segment, "01", "1", separators);
+        addElement(segment, "02", "1", separators);
+        gsLoop.addSegment(segment);
+        isaLoop.getLoops().add(gsLoop);
+        
+        
+        X12Writer writer = new X12Writer(FileType.ANSI837_5010_X222, Collections.singletonList(isaLoop), separators);
+        //System.out.println(writer.toX12String(LineBreak.CRLF));
+    }
+    
+    
+    private void addElement(Segment segment, String elementNum, String data, Separators separators) {
+        segment.addElement(new Element(segment.getId() + elementNum, data, separators));   
+    }
 }
