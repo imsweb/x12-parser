8adf4b5be4c50b79765ce3b59863b08319bb00df
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2020-07-24 15:20:50 -0500

    imports

--- src/main/java/com/imsweb/x12/Segment.java
+++ src/main/java/com/imsweb/x12/Segment.java
@@ -1,10 +1,8 @@
 package com.imsweb.x12;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
