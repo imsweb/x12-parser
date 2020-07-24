913450a86179c6a0bf0b9f62e8c612d6f860bd4e
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2020-07-24 15:11:03 -0500

    add missing dep

--- src/test/java/com/imsweb/x12/writer/X12WriterTest.java
+++ src/test/java/com/imsweb/x12/writer/X12WriterTest.java
@@ -13,8 +13,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import com.sun.deploy.util.StringUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
